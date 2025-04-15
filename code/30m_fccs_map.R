c2aabcdc86183da9e41781dd3cc0a2da80d8d307
jeremyash <jeremyd.ash@gmail.com>
2025-04-15 16:42:57 -0400

    added function for PR

--- code/30m_fccs_map.R
+++ code/30m_fccs_map.R
@@ -11,7 +11,7 @@ library(magick)
 library(htmltools)
 
 ##  function to create image of 30m fccs for PSAs  ----------------------------------------------
-
+## to do: redo function with part of country as input to specify raster dataset
 
 conus_fccs_map_fun <- function(UNIT, AGENCY) {
   
@@ -525,6 +525,178 @@ hi_fccs_map_fun <- function(UNIT, AGENCY) {
                                "_30m_fccs_map.png"))
 }
 
+AGENCY <- "PSA"
+UNIT <- "152"
+pr_fccs_map_fun <- function(UNIT, AGENCY) {
+  
+  # load disturbance codes
+  dist_codes <- read_csv("raw_data/DisturbanceCodes.csv") %>% 
+    fill(Type) %>% 
+    fill(Severity) %>%  
+    select(dist_id = DistCode,
+           type = Type) %>% 
+    mutate(dist_id = as.character(dist_id))
+  
+  # read in new and old FCCS data
+  conus <- rast("gis/LF2023_PRVI_240_IA/LF2023_FCCS_240_PRVI/Tif/LV23_FCCS_240.tif")
+  new_fac <- conus
+  
+  # aggregate new data to parent FCCS fuelbed and assign to new_fac
+  base_fccs <- cats(new_fac)[[1]] %>% 
+    select(FCCS, FCCSID, R, G, B) %>% 
+    rowwise() %>% 
+    mutate(FCCSID = gsub("_", "-", FCCSID)) %>% 
+    separate(FCCSID, into = c("base_fccs", "dist_id"), sep = "-") %>% 
+    mutate(base_fccs = ifelse(base_fccs == "Fill", FCCS, base_fccs)) %>% 
+    select(-dist_id) %>% 
+    mutate(base_fccs = as.numeric(base_fccs)) %>% 
+    arrange(base_fccs) %>% 
+    mutate(base_fccs = as.character(base_fccs)) 
+  
+  # create 
+  levels(new_fac) <- base_fccs
+  activeCat(new_fac) <- 1
+  
+  # read in df of colors for all beds
+  col_df <- read_csv("data/col_df.csv",
+                     col_types = list("c", "c")) %>% 
+    as.data.frame() 
+  
+  
+  # assign color values to both old and new data
+  coltab(new_fac) <- col_df
+  
+  # create dataframe of available gis admin data and filter to specified agency
+  sh_ls <- data.frame(agency = c("USFS", "NPS", "DoD", "BLM", "PSA"),
+                      path = c("gis/S_USA.AdministrativeForest",
+                               "gis/nps_boundary",
+                               "gis/tl_2019_us_mil",
+                               "gis/BLM_Natl_NLCS_National_Monuments_National_Conservation_Areas_Polygons_2978908493776641245.geojson",
+                               "gis/National_Predictive_Service_Areas_Boundaries.geojson"),
+                      unit_var = c("FORESTNAME", "UNIT_NAME", "FULLNAME", "NCA_NAME", "OBJECTID")) %>% 
+    filter(agency == AGENCY)
+  
+  # filter to AGENCY/UNIT
+  unit_unproj <- st_read(sh_ls$path) %>%
+    filter(get(sh_ls$unit_var) == UNIT)
+  unit <- st_transform(unit_unproj, crs = crs(conus)) %>%
+    mutate(ID = seq(1, n(), 1))
+  unit_area <- formatC(round(as.numeric(st_area(unit)*0.000247105), digits = 0), format = "d", big.mark = ",")
+  
+  # crop new rasters with unit
+  unit_new_cr <- crop(new_fac, unit, snap="out")                    
+  unit_new_fr <- rasterize(unit, unit_new_cr) 
+  unit_new <- mask(x=unit_new_cr, mask=unit_new_fr)
+  
+  # sum data by fuelbed x dataset
+  new_sum <- freq(unit_new)
+  
+  #calculate area for new fccs
+  new_sum_df <- new_sum %>% 
+    rename(fccs = value) %>% 
+    group_by(fccs) %>% 
+    summarise(new_count = sum(count, na.rm=TRUE)) %>% 
+    ungroup() %>% 
+    rowwise() %>% 
+    mutate(new_area_acres = new_count*30*30*0.000247105)
+  
+  
+  # write area to file
+  new_fccs_area_df <- new_sum_df %>% 
+    rename(base_fccs = fccs) %>% 
+    mutate(new_area_per = round(new_area_acres/sum(new_sum_df$new_area_acres)*100, digits = 0)) %>% 
+    mutate(new_area_char = ifelse(new_area_per < 1, "<1", as.character(new_area_per))) %>% 
+    arrange(desc(new_area_per)) %>% 
+    select(base_fccs,
+           area_percentage = new_area_char)
+  
+  write_csv(new_fccs_area_df,
+            paste0("federal_unit_reports_v2/",
+                   AGENCY,
+                   "/",
+                   UNIT,
+                   "_30m_fccs_fuelbed_area.csv"))
+  
+  
+  # create new legend table
+  new_leg_max <- ifelse(dim(new_sum_df)[1] <= 20, dim(new_sum_df)[1], 20)
+  new_col_df <- new_sum_df %>% 
+    rename(base_fccs = fccs) %>% 
+    mutate(new_area_per = round(new_area_acres/sum(new_sum_df$new_area_acres)*100, digits = 0)) %>% 
+    mutate(new_area_char = ifelse(new_area_per < 1, "<1", as.character(new_area_per))) %>% 
+    arrange(desc(new_area_per)) %>% 
+    left_join(., col_df) %>% 
+    mutate(blank = rep("", n())) %>% 
+    as.data.frame()
+  
+  new_col_leg <- new_col_df[1:new_leg_max, c("blank", "base_fccs", "new_area_char", "hex")]
+  
+  # text of other fuelbeds
+  new_other_fccs <- if(dim(new_col_df)[1] > 20) {
+    sort(as.numeric(new_col_df[c((new_leg_max + 1)):dim(new_col_df)[1], "base_fccs"]))} else{NULL}
+  
+  
+  new_other_statement <- ifelse(is.null(new_other_fccs),
+                                "", ifelse(length(new_other_fccs) == 1,
+                                           paste0(paste0("Other fuelbeds include: "), 
+                                                  paste0(new_other_fccs, ".")), 
+                                           paste0(paste0("Other fuelbeds include: "), 
+                                                  paste0(new_other_fccs[-length(new_other_fccs)],  collapse = ", "), 
+                                                  paste0(" and ", new_other_fccs[length(new_other_fccs)], "."))))
+  
+  # create kable table for new legend
+  new_leg <- kbl(new_col_leg[,1:3], booktabs = T, linesep = "",  col.names = linebreak(c(" ", "FCCS", "Area (%)")), align = "c", format = "html") %>%
+    kable_styling(full_width = F) %>%
+    kable_styling(font_size = 10, position = "center") %>% 
+    kable_styling(latex_options = "hold_position") %>% 
+    column_spec(1, color = "black", background = new_col_leg$hex, width = "0.5in") %>% 
+    column_spec(2, width = "0.5in") %>%
+    column_spec(3, width = "0.5in") %>%
+    footnote(general = new_other_statement, general_title = "", threeparttable = TRUE)  
+  
+  
+  
+  # write image to file
+  png(filename=paste0("temp_files/",
+                      UNIT,
+                      "_30m_fccs_map.png"),
+      height = 1000,
+      width = 1000)
+  terra::plot(unit_new, 
+              legend = FALSE, 
+              axes = FALSE, 
+              box = FALSE, 
+              maxcell = 5e8, 
+              oma=c(5,5,5,5),
+              mar = c(2,2,2,2))
+  plot(st_geometry(unit), add = TRUE)
+  dev.off()
+  
+  # save kable to png
+  save_kable(new_leg, 
+             file = paste0("temp_files/",
+                           UNIT,
+                           "_30m_fccs_legend_test.png"),
+             density = 1000,
+             zoom = 1.3)
+  
+  # Combine side by side with magick
+  map <- image_read(paste0("temp_files/",
+                           UNIT,
+                           "_30m_fccs_map.png"))
+  
+  legend <- image_read(paste0("temp_files/",
+                              UNIT,
+                              "_30m_fccs_legend_test.png"))
+  
+  combined <- image_append(c(map, legend))
+  image_write(combined, paste0("federal_unit_reports_v2/",
+                               AGENCY,
+                               "/",
+                               UNIT,
+                               "_30m_fccs_map.png"))
+}
+
 ## apply functions to PSAs ----------------------------------------------
 
 # read in PSA geojson and filter out polygons not in CONUS
@@ -545,6 +717,11 @@ hi_psa <- st_read("gis/National_Predictive_Service_Areas_Boundaries.geojson") %>
   st_drop_geometry() %>% 
   pull(OBJECTID)
 
+pr_psa <- st_read("gis/National_Predictive_Service_Areas_Boundaries.geojson") %>% 
+  filter(OBJECTID %in% c(seq(152,157,1))) %>% # HI
+  st_drop_geometry() %>% 
+  pull(OBJECTID)
+
 
 # apply render function to create reports
 lapply(as.character(conus_psa), function(x) conus_fccs_map_fun(x, "PSA"))
@@ -552,7 +729,7 @@ lapply(as.character(ak_psa), function(x) ak_fccs_map_fun(x, "PSA"))
 lapply(as.character(ak_psa[11:22]), function(x) ak_fccs_map_fun(x, "PSA"))
 lapply(as.character(hi_psa), function(x) hi_fccs_map_fun(x, "PSA"))
 
-lapply(as.character(hi_psa), function(x) hi_fccs_map_fun(x, "PSA"))
+
 
 hi_fccs_map_fun("116", "PSA") # doesn't work
 hi_fccs_map_fun("112", "PSA") # doesn't work
@@ -561,5 +738,5 @@ hi_fccs_map_fun("114", "PSA") # doesn't work
 hi_fccs_map_fun("115", "PSA") #doesn't work
 
 
-
+lapply(as.character(pr_psa), function(x) pr_fccs_map_fun(x, "PSA"))
 
